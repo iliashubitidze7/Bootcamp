e22d48bdd2f3e518b475ae95e72a22d5178bfa4e
ilia <shubitidzeilia7@gmail.com>
2025-03-15 16:11:47 +0400

    finished day-3

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Post
 from .models import Profile
+from .models import Product
+from .models import Category
+from .models import Comment
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Profile)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Comment)
+
--- core/models.py
+++ core/models.py
@@ -13,6 +13,14 @@ class Post(models.Model):
         return self.title
     
 
+class Comment(models.Model):
+    desciption = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') 
+
+    def __str__(self):
+        return f"Comment on {self.post.title}"
+    
+
 class Profile(models.Model):
     name = models.CharField(max_length=250, default ='ilia')
     lastname = models.CharField(max_length=250, default ='shubitidze')
@@ -23,4 +31,23 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.name} {self.lastname} , Age: {self.age}, Job: {self.job}, DOB: {self.date_of_birth}"
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, default= 'Soft Drinks')
+    desctiption = models.TextField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    desctiption = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
